eb18e49bd64ae9528f2ff6389a00f3b1d3c4d07f
Chamodi <chamodiindrejith14@gmail.com>
2024-11-24 22:23:11 +0530

    Adding Animal Matching Feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import { useState } from 'react';
-import './App.css';
-import Animal from './components/Animal';
-import {animals} from './data/AnimalsDb';
+import { useState } from "react";
+import "./App.css";
+import Animal from "./components/Animal";
+import AnimalsTable from "./components/AnimalsTable";
+import Result from "./components/Result";
+import { animals } from "./data/AnimalsDb";
 
 function App() {
   const selectRandomAnimal = () => {
     const randomIndex = Math.floor(Math.random() * animals.length);
     return animals[randomIndex].name;
-  }
+  };
 
   const [randomAnimal, setRandomAnimal] = useState(selectRandomAnimal());
+  const [selectedAnimal, setSelectedAnimal] = useState("");
+  const [result, setResult] = useState("");
+
+  const checkResult = (selectedAnimal) => {
+    if (selectedAnimal === randomAnimal) {
+      setResult("Win!");
+
+      setTimeout(() => {
+        setResult("");
+        setRandomAnimal(selectRandomAnimal());
+      }, 5000);
+    } else {
+      setResult("Lose!");
+    }
+  };
   return (
     <div className="container">
       <header className="border-orange">
         <h1>Animal Matching Game</h1>
       </header>
-      <div className="border">
-        <Animal randomAnimal={randomAnimal}
-        setRandomAnimal={setRandomAnimal}
-        animals ={animals}/>
+
+      <div className="board">
+        <Animal
+          randomAnimal={randomAnimal}
+          setRandomAnimal={setRandomAnimal}
+          animals={animals}
+        />
+
+        <Result result={result} />
+        <AnimalsTable
+          animals={animals}
+          checkResult={checkResult}
+          selectedAnimal={selectedAnimal}
+          setSelectedAnimal={setSelectedAnimal}
+        />
       </div>
     </div>
   );
